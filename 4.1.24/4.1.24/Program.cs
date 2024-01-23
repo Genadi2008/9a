6262ba5741f61b08095221bbc903171a4e0aa225
Genadi2008 <157457041+Genadi2008@users.noreply.github.com>
2024-01-23 09:52:40 +0200

    Add files via upload

---
+++ 4.1.24/4.1.24/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._1._24
+{ 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("vuvedete broq 4oveka v 4as");
+            int kolko4ovekasav4as = int.Parse(Console.ReadLine());
+
+            Console.Write("vuvedi koi 4as si");
+            int cas = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("vuvedi broi momicheta");
+            int momiche = int.Parse(Console.ReadLine());
+
+            Console.WriteLine( "casut e{ 0}/ horata ste {1}/ mom4etata ste");
+           
+
+            int momcheta = kolko4ovekasav4as - momiche;
+            
+
+                
+
+
+            
+
+                
+                
+
+
+        }
+        }
+    }
