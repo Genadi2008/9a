c89dd075ca803cd33c78c6bc3b63525df812b79e
Genadi2008 <157457041+Genadi2008@users.noreply.github.com>
2024-01-23 09:54:41 +0200

    Add files via upload

---
+++ 11.1.24/11.1.24/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11._1._24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var g = double.Parse(Console.ReadLine());
+            string pol = Console.ReadLine();
+            if (g < 16)
+            {
+                if (pol == "m") Console.WriteLine("mster");
+                else
+                if (pol == "f") Console.WriteLine("miss");
+                {
+                    if 
+                }
+            }
+
+        }         
+    }
+}
