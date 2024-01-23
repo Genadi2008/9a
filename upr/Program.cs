088042c0ff82c02a21fdfb9339aa70bb0fac88ac
Genadi2008 <157457041+Genadi2008@users.noreply.github.com>
2024-01-23 09:37:13 +0200

    Add files via upload

---
+++ upr/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace upr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("въведи x");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.Write("въведи y");
+            int y = int.Parse(Console.ReadLine());
+            if (x > 0 && 0 < y)
+
+                x = y; y = x;
+        Console.WriteLine("крадрад 2");
+
+
+        }
+    }
+}
